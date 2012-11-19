410f524971eac36329982edb80c35ed31eb39d4a
HyunSoo Park <dkfdnd@naver.com>
2012-11-19 14:03:28 +0900

    DAO DTO 테스트

--- src/test/test.java
+++ src/test/test.java
@@ -1,24 +1,27 @@
 package test;
 
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import dao.UserDAO;
-import dto.UserDTO; 
-import java.io.*;
-import java.util.List;
+import dto.UserDTO;
 
-import org.apache.ibatis.io.*;
-import org.apache.ibatis.session.*;
-
-public class test {
-	public UserDTO getUser(){
-			UserDAO dao = new UserDAO();
-			UserDTO list = null;
-			 
-		   try {
-			   list = dao.getUsers();
-		   } catch(Exception e){
-		   e.printStackTrace();
-		   }
+public class test extends HttpServlet {
+	public UserDTO getUser() {
+		UserDAO dao = new UserDAO();
+		UserDTO list = dao.getUsers();
 		return list;
-		 
+	}
+	
+public void doGet(HttpServletRequest request, HttpServletResponse response){
+		try {
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
 }
-}
+
